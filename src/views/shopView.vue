993435639dd413e78d6652fc4f742a4534bda6b6
Penny Chang <yahusanchang@gmail.com>
2022-09-25 16:52:15 +0800

    新增handleUrl()判斷return router id 值

--- src/views/shopView.vue
+++ src/views/shopView.vue
@@ -22,10 +22,10 @@
           <hr class="my-5">
           <div class="d-flex justify-content-space-between" @click="formStatusBtn">
             <button class="btn btn--prev text-center" :class="{'disabled': currentStatus === 0}">
-              <router-link :to="{name: 'shop', params: {id: handleUrl(currentStatus)}}" class="url-prev"><p class="m-0">上一步</p></router-link>
+              <router-link :to="{name: 'shop', params: {id: handleUrlPrev(currentStatus)}}" class="url-prev"><p class="m-0">上一步</p></router-link>
             </button>
             <button class="btn btn--next ml-auto text-center" :class="{'submit': submitBtnStatus === '確認下單'}">
-              <router-link :to="{name: 'shop', params: {id: currentStatus}}" class="url-next"><p class="m-0" @click="formSubmit">{{submitBtnStatus}}</p></router-link>
+              <router-link :to="{name: 'shop', params: {id: handleUrlNext(currentStatus)}}" class="url-next"><p class="m-0" @click="formSubmit">{{submitBtnStatus}}</p></router-link>
             </button>
           </div>
         </div>
@@ -138,11 +138,17 @@ export default{
         console.log(this.currentStatus)
       }
     },
-    handleUrl(num){
+    handleUrlPrev(num){
       if( num === 0){
         return 0
       }
       return num - 1
+    },
+    handleUrlNext(num){
+      if( num === 0){
+        return 1
+      }
+      return num 
     }
   },
   watch: {
